21cc6cb915c49524580a5f6dded71f4fad53a66f
Harutyun Hovsepyan <85588575+HarutyunHovsepyan@users.noreply.github.com>
2023-09-24 14:56:07 +0400

    all project

--- src/header/SearchCity.jsx
+++ src/header/SearchCity.jsx
@@ -1,8 +1,28 @@
-import "./SearchCity.css";
-import searchIcon from "../icons/search.svg";
-import WeatherResult from "../main/WeatherResult";
+import React, { useState} from 'react';
+import './SearchCity.css';
+import searchIcon from '../icons/search.svg';
+import WeatherResult from '../main/WeatherResult';
+import axios from 'axios';
 
 const SearchCity = () => {
+  const [city, setCity] = useState('');
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const apiKey = 'aed8a264ee06b21111a63106aa50b791';
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=${city}`;
+
+  const search = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      setWeatherData(response.data);
+      setError(null); 
+    } catch (error) {
+      setError('Invalid city name');
+      setWeatherData(null); 
+    }
+  };
+
   return (
     <div className="container">
       <div className="top-bar">
@@ -10,12 +30,15 @@ const SearchCity = () => {
           type="text"
           className="cityInput"
           placeholder="Enter Your city name"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
         />
-        <div className="search-icon">
+        <div className="search-icon" onClick={search}>
           <img src={searchIcon} alt="Search icon" />
         </div>
       </div>
-      <WeatherResult />
+      {error && <div className="error-message">{error}</div>}
+      <WeatherResult weatherData={weatherData} />
     </div>
   );
 };
--- src/main/WeatherResult.jsx
+++ src/main/WeatherResult.jsx
@@ -1,42 +1,59 @@
-import "./WeatherResult.css";
-import cloud from "../icons/cloud.svg";
-import humidity from "../icons/humidity.svg";
-import wind from "../icons/wind.svg";
+import React from 'react';
+import './WeatherResult.css';
+import clear from '../icons/clear.svg';
+import cloud from '../icons/cloud.svg';
+import haze from '../icons/haze.svg';
+import humidity from '../icons/humidity.svg';
+import rain from '../icons/rain.svg';
+import snow from '../icons/snow.svg';
+import storm from '../icons/storm.svg';
+import wind from '../icons/wind.svg';
+import mist from '../icons/mist.svg';
 
-const WeatherResult = () => {
+const WeatherResult = ({ weatherData }) => {
+
+  if (!weatherData) {
+    return <div className='city-name'>Please Enter City Name </div>;
+  }
+
+  const weatherCondition = weatherData.weather[0].main;
+
+  const weatherIcons = {
+    Clear: clear,
+    Clouds: cloud,
+    Haze: haze,
+    Rain: rain,
+    Snow: snow,
+    Thunderstorm: storm,
+    Wind: wind,
+    Mist: mist,
+  };
+
   return (
     <div className="weather">
       <div className="weather-image">
-        <img src={cloud} alt="Cloud" />
+        <img src={weatherIcons[weatherCondition]} alt={weatherIcons[weatherCondition]} />
       </div>
       <div className="weather-temp">
-        <p>24&deg;c</p>
+        <p>{Math.round(weatherData.main.temp)}&deg;C</p> 
       </div>
       <div className="weather-location">
-        <p>London</p>
+        <p>{weatherData.name}</p> 
       </div>
       <div className="data-container">
         <div className="element">
-            <img src={humidity} alt="" className="icon" />
-            <div className="data">
-                <div className="humidity-percent">
-                    64%
-                </div>
-                <div className="text">
-                    Humidity
-                </div>
-            </div>
+          <img src={humidity} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">{weatherData.main.humidity}%</div>
+            <div className="text">Humidity</div>
+          </div>
         </div>
         <div className="element">
-            <img src={wind} alt="" className="icon" />
-            <div className="data">
-                <div className="humidity-percent">
-                    18 km/h
-                </div>
-                <div className="text">
-                   Wind Speed
-                </div>
-            </div>
+          <img src={wind} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">{weatherData.wind.speed} km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
       </div>
     </div>
